434a9317b3671a69b503c891b0b16929dfa3b644
devhomie <173510882+devhomie@users.noreply.github.com>
2024-07-31 15:40:59 -0400

    Added comments to semi organize the code

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ let db = new sqlite.Database(
     console.log("Connected to the chinook database.");
   }
 );
-
+// *********** .all() METHOD EXAMPLE
 let sqlALL = `SELECT DISTINCT Name name FROM playlists
-           ORDER BY name`;
+ORDER BY name`;
 
 db.all(sqlALL, [], (err, rows) => {
   if (err) {
@@ -25,23 +25,28 @@ db.all(sqlALL, [], (err, rows) => {
     console.log(row.name);
   });
 });
+// *********** .all() METHOD EXAMPLE
 
+
+// *********** .get() METHOD EXAMPLE
 let sqlGET = `SELECT PlaylistId id,
-                  Name name
-           FROM playlists
-           WHERE PlaylistId  = ?`;
-let playlistId = 1;
- 
+Name name
+FROM playlists
+WHERE PlaylistId  = ?`;
+let playlistId = 5;
+
 // first row only
 db.get(sqlGET, [playlistId], (err, row) => {
   if (err) {
     return console.error(err.message);
   }
   return row
-    ? console.log(row.id, row.name)
-    : console.log(`No playlist found with the id ${playlistId}`);
-
+  ? console.log(row.id, row.name)
+  : console.log(`No playlist found with the id ${playlistId}`);
+  
 });
+// *********** .get() METHOD EXAMPLE
+
 
 db.close((err) => {
   if (err) {
