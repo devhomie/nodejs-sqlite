93729350981c44d3eab5b9009a0f9f1cebacd7b2
devhomie <173510882+devhomie@users.noreply.github.com>
2024-07-31 11:37:25 -0400

    imported sqlite3 to server.js
    Opened sqlite sample database (chinook.db) using 'OPEN_READWRITE'
    
    Lastly, queried the data from the 'playlists' table

---
+++ server.js
@@ -0,0 +1,33 @@
+import sqlite3 from "sqlite3"
+
+const sqlite = sqlite3.verbose();
+
+// open the database
+let db = new sqlite.Database(
+  "./chinook.db",
+  sqlite.OPEN_READWRITE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the chinook database.");
+  }
+);
+
+db.serialize(() => {
+  db.each(`SELECT PlaylistId as id,
+                  Name as name
+           FROM playlists`, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.id + "\t" + row.name);
+  });
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
